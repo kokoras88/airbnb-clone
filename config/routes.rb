c171b18b229c2315121914773cf5d5dc07ac3dec
Thiseas Boulakis <thiseas16@gmail.com>
2023-02-17 21:55:10 +0100

    created show and delete actions. installed and implemented pundit for authorization. Now you can only edit and delete the games that you are the owner of. The buttons for these actions also appear only if you are the owner. Home button now takes you to home. Navbar is also rendered in every view, except new.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :games, only: %i[index new create]
+  resources :games, only: %i[index new create show edit update destroy]
 end
